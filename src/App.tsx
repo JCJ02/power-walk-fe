7be048977b5034d38ebcc1caa448da8e3b0c32f2
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-24 18:04:17 +0800

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen w-full">
+        <h1 className="font-mono text-4xl uppercase">Power Walk Technology</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
