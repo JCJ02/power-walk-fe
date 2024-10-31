262a1c7bb0fbdfd838ce8bda327076d42d2b1d1f
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-01 01:38:40 +0800

    Feat: Add AppRoutes.ts File for Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "../pages/NotFound";
-import LogIn from "../pages/auth/LogIn";
+import LogIn from "../pages/auth/login/LogIn";
+import SignUp from "../pages/auth/signup/SignUp";
 
 const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="*" element={<NotFound />} />
-                <Route path="/login" element={<LogIn />} />
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
