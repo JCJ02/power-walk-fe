d1d75ee0db4ca7f22d93dcfd86f666c93b85e902
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-01 01:35:18 +0800

    Fix: Adjust Padding

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,22 @@
-import Footer from "../layouts/Footer"
+import Footer from "../layouts/Footer";
 
 const NotFound = () => {
-    return (
-        <>
-            <div className="flex flex-col justify-center items-center h-screen w-full">
-                <h1 className="font-poppins font-extrabold text-7xl">404</h1>
-                <h2 className="font-poppins text-slate-600 text-2xl">Oops, This Page Not Found!</h2>
-                <h2 className="font-poppins text-sm text-slate-400">The link might be corrupted or the page may have been removed.</h2>
-                <Footer />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center px-5 h-screen w-full">
+        <h1 className="font-poppins font-extrabold text-2xl md:text-4xl lg:text-7xl">
+          404
+        </h1>
+        <h2 className="font-poppins text-slate-600 text-lg md:text-xl lg:text-2xl">
+          Oops, This Page Not Found!
+        </h2>
+        <h2 className="font-poppins text-xs md:text-sm lg:text-md text-center text-slate-400">
+          The link might be corrupted or the page may have been removed.
+        </h2>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
