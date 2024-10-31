98102317d2111a884817d1d02f62210f902043f7
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-01 02:02:21 +0800

    Feat: Add SignUp

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import LogIn from "../pages/auth/login/LogIn";
 import SignUp from "../pages/auth/signup/SignUp";
 
 const AppRoutes = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
