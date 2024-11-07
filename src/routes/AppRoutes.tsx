ddee71dec83189a48e66583738dfa16149374150
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-07 22:09:34 +0800

    Feat: Add Dashboard and Students in the Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import LogIn from "../pages/authentication/login/LogIn";
 import SignUp from "../pages/authentication/signup/SignUp";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Students from "../pages/students/Students";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +12,10 @@ const AppRoutes = () => {
         <Route path="*" element={<NotFound />} />
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/">
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="students" element={<Students />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
