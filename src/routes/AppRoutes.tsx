1f824042059700d081f2b69b4bd78cf90821cd97
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-08 18:40:56 +0800

    Feat: Add Some Pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,9 @@ import LogIn from "../pages/authentication/log-in/LogIn";
 import SignUp from "../pages/authentication/sign-up/SignUp";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Students from "../pages/students/Students";
+import ChangePassword from "../pages/settings/ChangePassword";
+import EditProfile from "../pages/settings/EditProfile";
+import Admin from "../pages/admin/Admin";
 
 const AppRoutes = () => {
   return (
@@ -12,9 +15,11 @@ const AppRoutes = () => {
         <Route path="*" element={<NotFound />} />
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/">
-          <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Admin />}>
+          <Route index element={<Dashboard />} />
           <Route path="students" element={<Students />} />
+          <Route path="change-password" element={<ChangePassword />} />
+          <Route path="edit-profile" element={<EditProfile />} />
         </Route>
       </Routes>
     </BrowserRouter>
