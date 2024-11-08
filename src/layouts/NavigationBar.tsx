68a576ba1016c764e41159d9b4290c3beeb967da
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-08 19:34:33 +0800

    Fix: Add Some Style in NavigationBar Components

--- src/layouts/NavigationBar.tsx
+++ src/layouts/NavigationBar.tsx
@@ -1,8 +1,19 @@
+import Picture from "../components/ui/Picture";
+import AdminPhoto from "../assets/images/Admin-Profile-Vector-PNG-Isolated-HD.png";
+
 const NavigationBar = () => {
   return (
     <>
-      <div className="bg-[#FEFEFE] h-[75px] w-full">
-        <label>Welcome, Admin</label>
+      <div className="bg-[#FEFEFE] flex items-center justify-end border-b-2 border-black h-[80px] w-full">
+        <div className="flex items-center gap-2 px-5 cursor-pointer">
+          <label className="font-poppins text-xs md:text-md lg:text-lg">
+            Welcome, Admin
+          </label>
+          <Picture
+            className="rounded-full h-[20px] w-[20px] lg:h-[30px] lg:w-[30px]"
+            source={AdminPhoto}
+          />
+        </div>
       </div>
     </>
   );
