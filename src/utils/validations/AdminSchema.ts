34d76b1353680c38b7613594aba641710080b086
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-14 02:06:12 +0800

    Fix: Change Variable Names

--- src/utils/validations/AdminSchema.ts
+++ src/utils/validations/AdminSchema.ts
@@ -1,16 +1,18 @@
 import { z } from "zod";
 
-const loginSchema = z.object({
+const logInSchema = z.object({
     email: z.string({
-        required_error: "E-mail Is Required!"
+        required_error: "E-mail Is Required!",
+        invalid_type_error: "Invalid E-mail Address!"
     }).email("Must Be A Valid E-Mail Address!"),
     password: z.string({
         required_error: "Password Is Required!"
     }).min(8, "Password Must Be At Least 8 Characters Long!")
+        .regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])/, "Password Must Containt At Least One Number And One Special Character!")
 });
 
-export type Login = z.infer<typeof loginSchema>;
+export type LogInType = z.infer<typeof logInSchema>;
 
 export {
-    loginSchema,
+    logInSchema,
 }
