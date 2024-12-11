16996ca2053168daee96ddf8dda980dd2c2f0e14
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-12-12 01:46:56 +0800

    Fix: Config RQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./routes/AppRoutes";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 2,
+        staleTime: 1000 * 60 * 5,
+      },
+    },
+  });
 
   return (
     <>
@@ -13,7 +21,7 @@ function App() {
         <ToastContainer />
       </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
