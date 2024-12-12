75f66dec7747fd0089b66f22960a2db1a2690496
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-12-12 22:00:30 +0800

    Feat: Implement Modal Components

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,26 @@
-const Modal = () => {
-  return <>Modal</>;
+import { cn } from "../lib/utils";
+import React from "react";
+
+type ModalProps = {
+  isOpen?: boolean;
+  onClose?: any;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
+  if (!isOpen) return null;
+  return (
+    <div
+      className={cn(
+        "fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50",
+        className
+      )}
+      onClick={onClose}
+    >
+      <div onClick={(event) => event.stopPropagation()}>{children}</div>
+    </div>
+  );
 };
 
 export default Modal;
