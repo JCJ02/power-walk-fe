fceebc0605388c9ed1e28a96a451b6728885a0a6
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-01-31 21:47:44 +0800

    Feat: Create custom NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import Footer from "../layouts/Footer";
+import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
 
 const NotFound = () => {
   useEffect(() => {
@@ -8,13 +10,25 @@ const NotFound = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center px-5 h-screen w-full">
+        <Logo />
         <h1 className="font-poppins font-extrabold text-7xl">404</h1>
-        <h2 className="font-poppins text-slate-600 text-lg md:text-xl lg:text-2xl">
+        <h2 className="font-poppins text-[#385A65] text-lg md:text-xl lg:text-2xl">
           Oops, This Page Not Found!
         </h2>
-        <h2 className="font-poppins text-xs md:text-sm lg:text-md text-center text-slate-400">
+        <p className="font-poppins text-xs md:text-sm lg:text-md text-center text-slate-400">
           The link might be corrupted or the page may have been removed.
-        </h2>
+        </p>
+        <div className="flex items-center gap-2 mt-10">
+          <p className="font-poppins text-xs md:text-md lg:text-lg">
+            Go Back To
+          </p>
+          <Link
+            className="font-poppins font-bold text-xs md:text-md lg:text-lg text-[#385A65] hover:underline"
+            to={"/log-in"}
+          >
+            Sign In
+          </Link>
+        </div>
         <Footer />
       </div>
     </>
