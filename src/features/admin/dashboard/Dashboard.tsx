dc5bf52f505e21c0c06211986fd7f0bfb8534c5e
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-01-31 21:54:49 +0800

    Feat: Create dashboard

--- src/features/admin/dashboard/Dashboard.tsx
+++ src/features/admin/dashboard/Dashboard.tsx
@@ -6,7 +6,10 @@ const Dashboard = () => {
   }, []);
   return (
     <>
-      <div className="h-full w-full">Dashboard</div>
+      <div className="flex flex-col items-start font-poppins py-5 pl-6 lg:pl-8 pr-6 lg:pr-10 overflow-y-scroll h-screen w-full">
+        <h1 className="text-xl xl:text-3xl font-semibold">Dashboard</h1>
+        <div className="w-full"></div>
+      </div>
     </>
   );
 };
