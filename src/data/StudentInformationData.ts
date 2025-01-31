89ae3ee557eedabab5ad82dd33ed106829db04b6
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-01-31 21:39:36 +0800

    Feat: Create student information data for sample only

---
+++ src/data/StudentInformationData.ts
@@ -0,0 +1,34 @@
+export const StudentInformationData = [
+    {
+        id: 1,
+        studentId: "21-1780",
+        studentName: "Jacobe, John Carlo P.",
+        emailAddress: "jacobe.johncarlo.02022003@gmail.com",
+        dateOfBirth: "Feb. 02, 2003",
+        address: "49-P Sampaloc Alley Hills St. Batasan Hills, Quezon City"
+    },
+    {
+        id: 2,
+        studentId: "21-1780",
+        studentName: "Jacobe, John Carlo P.",
+        emailAddress: "jacobe.johncarlo.02022003@gmail.com",
+        dateOfBirth: "Feb. 02, 2003",
+        address: "49-P Sampaloc Alley Hills St. Batasan Hills, Quezon City"
+    },
+    {
+        id: 3,
+        studentId: "21-1780",
+        studentName: "Jacobe, John Carlo P.",
+        emailAddress: "jacobe.johncarlo.02022003@gmail.com",
+        dateOfBirth: "Feb. 02, 2003",
+        address: "49-P Sampaloc Alley Hills St. Batasan Hills, Quezon City"
+    },
+    {
+        id: 4,
+        studentId: "21-1780",
+        studentName: "Jacobe, John Carlo P.",
+        emailAddress: "jacobe.johncarlo.02022003@gmail.com",
+        dateOfBirth: "Feb. 02, 2003",
+        address: "49-P Sampaloc Alley Hills St. Batasan Hills, Quezon City"
+    }
+]
