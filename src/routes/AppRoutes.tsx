82e2471621b2a923183fedafce381a4b7e6e0b48
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-02-02 00:02:11 +0800

    Fix: Delete Test

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -7,9 +7,6 @@ import StudentInformation from "../features/admin/student-information/StudentInf
 import ChangePassword from "../features/settings/ChangePassword";
 import EditProfile from "../features/settings/EditProfile";
 import Admin from "../features/admin/Admin";
-import Test from "../features/__test__/Test";
-import Test1 from "../features/__test__/Test1";
-import TestReactQuery from "../features/__test__/TestReactQuery";
 
 const AppRoutes = () => {
   return (
@@ -18,9 +15,6 @@ const AppRoutes = () => {
         <Route path="*" element={<NotFound />} />
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/test-1" element={<Test1 />} />
-        <Route path="/test-react-query" element={<TestReactQuery />} />
         <Route path="/" element={<Admin />}>
           <Route index element={<Dashboard />} />
           <Route path="student-information" element={<StudentInformation />} />
