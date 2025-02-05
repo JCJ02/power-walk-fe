94f24912ae8f98a4b7377cc8f55ecbaf28007d34
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-02-06 03:06:41 +0800

    Feat: Implement create and update student schema using zod

--- src/utils/zod/StudentSchema.ts
+++ src/utils/zod/StudentSchema.ts
@@ -23,8 +23,30 @@ const createStudentSchema = z.object({
     address: z.string().min(1, "Address is Required!")
 });
 
+const updateStudentSchema = z.object({
+    studentId: z.string()
+        .min(1, "Student ID is Required!"),
+    firstname: z.string()
+        .min(1, "Firstname is Required!")
+        .max(255, "Firstname must not Exceed to 255 Characters Long!"),
+    lastname: z.string()
+        .min(1, "Lastname is Required!")
+        .max(255, "Lastname must not Exceed to 255 Characters Long!"),
+    middlename: z.string().optional().nullable(),
+    email: z.string().email("Must be a Valid Email Address!")
+        .min(1, "Email is Required!")
+        .max(255, "Email must not Exceed to 255 Characters Long!"),
+    dateOfBirth: z.coerce.date()
+        .refine((date) => !isNaN(date.getTime()), {
+            message: "Invalid Date. Please Enter a Valid Date.",
+        }),
+    address: z.string().min(1, "Address is Required!")
+});
+
 export type NewStudentType = z.infer<typeof createStudentSchema>;
+export type UpdateStudentType = z.infer<typeof updateStudentSchema>;
 
 export {
     createStudentSchema,
+    updateStudentSchema
 }
