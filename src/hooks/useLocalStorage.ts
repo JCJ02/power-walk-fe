64f4907a639f8897c2cf721ec876a7a114160b1b
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-02-05 12:43:39 +0800

    Fix: Bugs

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,28 +1,33 @@
 import { useState } from "react";
 
 const useLocalStorage = <T>(key: string, initialValue: T) => {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      const item = localStorage.getItem(key);
-      return item ? (JSON.parse(item) as T) : initialValue;
-    } catch (error) {
-      console.error(`Error Reading LocalStorage Key "${key}":`, error);
-      return initialValue;
-    }
-  });
 
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.error(`Error Setting LocalStorage Key "${key}":`, error);
-    }
-  };
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        try {
+            const item = localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error: any) {
+            console.error(`Error Reading LocalStorage Key "${key}":`, error);
+            return initialValue;
+        }
+    });
 
-  return [storedValue, setValue] as const;
-};
+    const setValue = (value: T | ((val: T) => T)) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            if (typeof valueToStore === "string") {
+                localStorage.setItem(key, valueToStore);
+            } else {
+                localStorage.setItem(key, JSON.stringify(valueToStore));
+            }
+        } catch (error: any) {
+            console.error(`Error Setting LocalStorage Key "${key}":`, error);
+        }
+    }
+
+    return [storedValue, setValue] as const;
+
+}
 
 export default useLocalStorage;
