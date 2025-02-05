03c44e2021913aef82c863ab1f5648ae3d8dd2d2
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-02-05 12:45:10 +0800

    Feat: Create useNewStudentMutation hook for creating student

---
+++ src/features/admin/student-information/hooks/useNewStudentMutation.ts
@@ -0,0 +1,45 @@
+import { Bounce, toast } from 'react-toastify';
+import { useQueryClient } from "@tanstack/react-query";
+import baseUrl from '../../../../utils/baseUrl';
+import usePost from '../../../../hooks/usePost';
+import { NewStudentType } from '../../../../utils/zod/StudentSchema';
+
+type NewStudentResponse = {
+    id: number,
+    uid: string,
+    studentId: string,
+    firstname: string,
+    lastname: string,
+    middlename?: string,
+    email: string,
+    dateOfBirth: Date;
+    address: string
+}
+
+const useNewStudentMutation = () => {
+    const UseQueryClient = useQueryClient();
+    const newStudentMutation = usePost<NewStudentType, NewStudentResponse>({
+        url: `${baseUrl}api/student/`,
+        requiresAuthentication: true,
+        onSuccess: () => {
+            toast.success('Successfully Created!', {
+                toastId: "createStudentSuccessfully",
+                position: "top-right",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            });
+            UseQueryClient.invalidateQueries({queryKey: ["students"]});
+        }
+    });
+
+    return newStudentMutation;
+
+}
+
+export default useNewStudentMutation;
