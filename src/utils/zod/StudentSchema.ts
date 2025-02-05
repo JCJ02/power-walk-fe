bfd6fcf76f0effad9a79ea78084e87d8da56fe55
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-02-05 12:43:25 +0800

    Fix: Change the type of uid to string

--- src/utils/zod/StudentSchema.ts
+++ src/utils/zod/StudentSchema.ts
@@ -1,25 +1,30 @@
 import { z } from "zod";
 
-const studentSchema = z.object({
-    rfidNumber: z.number().min(1, "RFID Number Is Required!")
-        .max(255, "RFID Number Must Not Exceed To 255 Characters!"),
-    studentId: z.string().min(7, "Student ID Is Required!")
-        .max(255, "Student ID Must Not Exceed To 255 Characters!"),
-    lastname: z.string().min(1, "Lastname Is Required!")
-        .max(255, "Lastname Must Not Exceed To 255 Characters!"),
-    firstname: z.string().min(1, "Firstname Is Required!")
-        .max(255, "Firstname Must Not Exceed To 255 Characters!"),
-    emailAddress: z.string({
-        required_error: "E-mail Is Required!"
-    }).email("Must Be A Valid E-Mail Address!"),
-    // dateOfBirth: z.coerce.date(),
-    dateOfBirth: z.string().min(1, "Birthdate Is Required!"),
-    address: z.string().min(1, "Address Is Required!")
-        .max(255, "Address Must Not Exceed To 255 Characters!"),
+const createStudentSchema = z.object({
+    uid: z.string({
+        invalid_type_error: "Description Must Be String!"
+    }).min(1, "RFID is Required!"),
+    studentId: z.string()
+        .min(1, "Student ID is Required!"),
+    firstname: z.string()
+        .min(1, "Firstname is Required!")
+        .max(255, "Firstname must not Exceed to 255 Characters Long!"),
+    lastname: z.string()
+        .min(1, "Lastname is Required!")
+        .max(255, "Lastname must not Exceed to 255 Characters Long!"),
+    middlename: z.string().optional().nullable(),
+    email: z.string().email("Must be a Valid Email Address!")
+        .min(1, "Email is Required!")
+        .max(255, "Email must not Exceed to 255 Characters Long!"),
+    dateOfBirth: z.coerce.date()
+        .refine((date) => !isNaN(date.getTime()), {
+            message: "Invalid Date. Please Enter a Valid Date.",
+        }),
+    address: z.string().min(1, "Address is Required!")
 });
 
-export type StudentType = z.infer<typeof studentSchema>;
+export type NewStudentType = z.infer<typeof createStudentSchema>;
 
 export {
-    studentSchema
+    createStudentSchema,
 }
